0eb7259a600cc85ac64e622686ffee96e359dc80
Gustavo Sarapura <gustavo.sarapura7@gmail.com>
2022-08-31 20:13:02 -0300

    Add UDP scan

--- scanner-nmap.py
+++ scanner-nmap.py
@@ -11,14 +11,27 @@ ip_addr=input("Inserte la IP por scanear: ")
 print("La IP que ingreso:", ip_addr)
 
 resp = input("""\nPor favor, seleccionar el tipo de escaneo:
-            1) Escaneo de sistema operativo
+            1) Escaneo TCP
             2) Escaneo UDP \n""")
 print("Su selección:", resp)
 
 if resp == '1':
     print("Version de nmap: ", scanner.nmap_version())
-    scanner.scan(ip_addr, '1-1024', '-O', '-v')
+    scanner.scan(ip_addr, '1-1024', '-v') # -v goes faster.
     print(scanner.scaninfo())
     print("Estado de IP: ", scanner[ip_addr].state())
     print(scanner[ip_addr].all_protocols())
     print("Puertos abiertos: ",scanner[ip_addr]['tcp'].keys())
+elif resp == '2':
+    print("Version de nmap: ", scanner.nmap_version())
+    scanner.scan(hosts = ip_addr, arguments = '-sU -v -p 1-444')
+    print(scanner.scaninfo())
+    print("Estado de IP: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    # change this: print("Puertos abiertos: ",scanner[ip_addr]['udp'].keys())
+    open_ports = []
+    ports = scanner[ip_addr]['udp']
+    for key in ports.keys():
+        if ports[key]['state'] == 'open':
+            open_ports.append(key)
+    print("Puertos abiertos: ", open_ports)
