cad8f17997ad2c5bab329944b1abb88f69be4ffb
Gustavo Sarapura <gustavo.sarapura7@gmail.com>
2022-09-09 17:52:52 -0300

    Modify details

--- scanner-nmap.py
+++ scanner-nmap.py
@@ -1,14 +1,12 @@
 #! /usr/bin/python
 
 import nmap
-import sys
-import socket
 
-scanner=nmap.PortScanner()
+scanner = nmap.PortScanner()
 
-print("Bienvenido a mi primer escaneo.")
-ip_addr=input("Inserte la IP por scanear: ")
-print("La IP que ingreso:", ip_addr)
+print("*** Bienvenido a mi primer escaneo ***")
+ip_addr = input("Inserte la IP por scanear: ")
+print("La IP que ingresó:", ip_addr)
 
 resp = input("""\nPor favor, seleccionar el tipo de escaneo:
             1) Escaneo TCP
@@ -20,18 +18,23 @@ if resp == '1':
     scanner.scan(ip_addr, '1-1024', '-v') # -v goes faster.
     print(scanner.scaninfo())
     print("Estado de IP: ", scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
-    print("Puertos abiertos: ",scanner[ip_addr]['tcp'].keys())
+    if scanner[ip_addr].all_protocols() != []:
+        print("Puertos abiertos: ",scanner[ip_addr]['tcp'].keys())
+    else:
+        print("Los puertos están cerrados.")
 elif resp == '2':
     print("Version de nmap: ", scanner.nmap_version())
-    scanner.scan(hosts = ip_addr, arguments = '-sU -v -p 1-444')
+    scanner.scan(hosts = ip_addr, arguments = '-sU -v -p 1-1024') # UDP scan takes quiet some time, be patient.
     print(scanner.scaninfo())
     print("Estado de IP: ", scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
-    # change this: print("Puertos abiertos: ",scanner[ip_addr]['udp'].keys())
     open_ports = []
     ports = scanner[ip_addr]['udp']
     for key in ports.keys():
         if ports[key]['state'] == 'open':
             open_ports.append(key)
-    print("Puertos abiertos: ", open_ports)
+    if open_ports != []:
+        print("Puertos abiertos: ", open_ports)
+    else:
+        print("Los puertos están cerrados.")
+else:
+    print("Ingrese una opción correcta")
