a9c5e463d1ff3ba6940df7991d7f55a8aac493b1
Gustavo Sarapura <gustavo.sarapura7@gmail.com>
2022-09-08 22:04:18 -0300

    Update host_discovery with input

--- host_discovery.py
+++ host_discovery.py
@@ -4,9 +4,10 @@ import nmap
 
 scanner = nmap.PortScanner()
 
-# Host discovery:
-hosts = scanner.scan(hosts='192.168.0.0/24', arguments='-sn')
+network = input('Please, enter the network in CIDR notationt (example: 192.168.122.0/24): ')
+
+hosts = scanner.scan(hosts = network, arguments ='-sn')
 hosts_list = [(x, scanner[x]['status']['state']) for x in scanner.all_hosts()]
 for host, status in hosts_list:
     if status == 'up':
-        print(host, status)
+        print('Host:', host,'- status:', status)
